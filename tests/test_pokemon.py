6bfb42861e22a164a94efbd57a08b81713a612f8
Starscourge <atacandogan13@gmail.com>
2026-07-13 10:16:55 +0200

    added mock test, with description

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -13,7 +13,17 @@ from togepy.models.pokemon import Pokemon, PokeTeam
 MAX_TEAM_SIZE = 6
 
 #All das wo api caller benutzt wird oder sonst was mit mock/dummy/stub/double?
+
 #Unit Test
+def test_poketeam_len():
+    #Arrange
+    team = PokeTeam("Team 1")
+    testpoke = Pokemon(1, "ditto", "", "")
+    #Act
+    team = add_pokemon_to_team(team, testpoke)
+    #Assert
+    assert len(team.pokemons) == 1
+
 def test_change_ability():
     #Arrange
     caller = APICaller()
@@ -40,12 +50,6 @@ def test_sanitize_dict():
                            "maintype": "Normal",
                            "sectype": None}
 
-#Unit Test
-def test_poketeam_len():
-    team = PokeTeam("Team 1")
-    testpoke = Pokemon(1, "ditto", "", "")
-    team = add_pokemon_to_team(team, testpoke)
-    assert len(team.pokemons) == 1
 
 #Unit Test
 def test_poketeam_maxlen():
@@ -74,3 +78,40 @@ def test_init_pokemon_obj():
         miss_counter += 1
 
     assert miss_counter == 0
+
+#Mock
+def test_change_ability_mock(mocker):
+    #Mock von API Caller erstellen
+    mock_api_caller = mocker.Mock()
+
+    #Ergebnis von apicaller.get_pokemon_name mocken
+    mock_result = {
+        "id": 132,
+        "name": "ditto",
+        "types": [
+            {
+                "slot": 1,
+                "type": {
+                    "name": "normal",
+                }
+            }
+        ],
+        "abilities": [
+            {
+                "ability": {
+                    "name": "transform"
+                }
+            }
+        ]
+    }
+
+    #return wert der funktion auf unseren mock setzen
+    mock_api_caller.get_pokemon_name.return_value = mock_result
+
+    #zu testende funktion mit mock_api_caller aufrufen
+    result = change_ability(mock_api_caller, Pokemon(1, "ditto", "", ""))
+    assert result == ["transform"]
+
+    #prüfen, ob die funktion genau ein mal aufgerufen wurde
+    #Macht Mock zum Mock, und nicht zum Stub: Mock prüft zusätzlich Verhalten
+    mock_api_caller.get_pokemon_name.assert_called_once_with("ditto")
